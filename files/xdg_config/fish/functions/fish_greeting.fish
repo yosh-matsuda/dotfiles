5c4a924082dbfcbf3652e9d48b131fa4caa0b503
Yoshiki Matsuda <y_matsuda@fixstars.com>
2026-05-16 15:20:18 +0900

    ✨ Add fastfetch configuration and update fish greeting function

--- files/xdg_config/fish/functions/fish_greeting.fish
+++ files/xdg_config/fish/functions/fish_greeting.fish
@@ -1,9 +1,24 @@
 function fish_greeting
+    if test "$TERM_PROGRAM" = "vscode"
+        return
+    end
+
+    # oh-my-logo
+    if command -q npx
+        set palettes grad-blue sunset dawn nebula mono ocean fire forest gold purple mint coral matrix
+        set palette_index (random 1 (count $palettes))
+        npx -y --silent oh-my-logo (hostname) $palettes[$palette_index] --filled --block-font tiny --letter-spacing 3
+    end
+
     # message of the day
-    for motd in /run/motd.dynamic /etc/motd
-        if test -s "$motd"
-            /bin/cat "$motd"
-            break
+    if command -q fastfetch
+        fastfetch
+    else
+        for motd in /run/motd.dynamic /etc/motd
+            if test -s "$motd"
+                /bin/cat "$motd"
+                break
+            end
         end
     end
 end
